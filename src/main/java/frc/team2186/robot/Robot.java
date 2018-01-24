663daac983ac759975a9bbe67e617e1523ff9b9d
Kenny Raisbeck <drwaterycat@gmail.com>
2018-01-24 18:06:27 -0500

    Initial Commit

---
+++ src/main/java/frc/team2186/robot/Robot.java
@@ -0,0 +1,15 @@
+package frc.team2186.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+}
